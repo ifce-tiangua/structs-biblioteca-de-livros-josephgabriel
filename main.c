2faaffb3b0af431ec62e4dd454ed520e6c6b2a4b
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-25 20:06:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,86 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){
+typedef struct livro {
+	
+	char nome[100];
+	char isbn[20];
+	float preco;
+	int score;
+	char editora[50];
+} plivro;
 
-    //implemente seu codigo
-    printf("Nao implementado ainda.");
+
+//1. Alocar Memória
+
+    plivro* livro_aloc(int qtde) {
     
+    plivro* vetorLivros = (plivro*)malloc(qtde * sizeof(plivro));
+
+    return vetorLivros;
+}
+
+//2. Ler Dados
+
+void livro_ler(plivro* livros){
+	
+	fgets(livros->nome, sizeof(livros->nome), stdin);
+	livros->nome[strcspn(livros->nome, "\n")] = '\0';
+	
+	fgets(livros->isbn, sizeof(livros->isbn), stdin);
+    livros->isbn[strcspn(livros->isbn, "\n")] = '\0';
+	
+	scanf("%f", &livros -> preco);
+	getchar();
+	
+	scanf("%d", &livros -> score);
+	getchar();
+	
+    fgets(livros->editora, sizeof(livros->editora), stdin);
+    livros->editora[strcspn(livros->editora, "\n")] = '\0';
+}
+
+//3. Exibir Dados
+
+void livro_exibe(plivro* livros){
+	printf("Nome: %s\n", livros -> nome);
+	printf("ISBN: %s\n", livros -> isbn);
+	printf("Preço: R$ %.2f\n", livros -> preco);
+	printf("Score: %d\n",  livros -> score);
+	printf("Editora: %s\n", livros -> editora);
+	
+}
+
+//4. Liberar Memória
+
+void livro_desaloca(plivro* livros){
+	
+	free(livros);
+	
+}
+
+int main() {
+	
+    int qtdeLivros;
+
+    scanf("%d", &qtdeLivros);
+    getchar();
+
+    plivro* livros = livro_aloc(qtdeLivros);
+
+    for (int i = 0; i < qtdeLivros; i++) {
+        livro_ler(&livros[i]);
+    }
+
+    for (int i = 0; i < qtdeLivros; i++) {
+        printf("\nLivro %d:\n", i + 1);
+        livro_exibe(&livros[i]);
+    }
+
+
+    livro_desaloca(livros);
+
     return 0;
 }
+
